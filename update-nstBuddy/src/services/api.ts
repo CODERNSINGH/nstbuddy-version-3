29274ba4d110be834cf90d5a06e3f4a8844ad0ed
Narendra Singh <narendrasingh.coder@gmail.com>
2026-08-12 16:34:57 +0530

    added API

--- update-nstBuddy/src/services/api.ts
+++ update-nstBuddy/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_BASE_URL = "https://nstbuddy-version-3.onrender.com/api";
 
 
 const api = axios.create({
